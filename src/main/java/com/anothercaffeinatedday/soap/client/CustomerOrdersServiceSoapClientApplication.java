fe5042ff59bb386e5cd0447ed325ec954f07e53f
Timothy Stone <github@petmystone.com>
2020-11-03 21:09:26 -0500

    Create SOAP Client from Spring Initializr
    Initial commit of project following generation.

---
+++ src/main/java/com/anothercaffeinatedday/soap/client/CustomerOrdersServiceSoapClientApplication.java
@@ -0,0 +1,13 @@
+package com.anothercaffeinatedday.soap.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOrdersServiceSoapClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerOrdersServiceSoapClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anothercaffeinatedday/soap/client/CustomerOrdersServiceSoapClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anothercaffeinatedday.soap.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerOrdersServiceSoapClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
